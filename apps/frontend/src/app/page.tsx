c4c850d077bb5e67c742440479a3cf42ac787989
Tamal <ckbtamaldipnew@gmail.com>
2025-03-13 21:45:59 +0600

    shadcn test done

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,3 +1,20 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Bold } from "lucide-react";
+
+import { Toggle } from "@/components/ui/toggle";
+
 export default function Home() {
-  return <div className=" text-4xl">Hi there </div>;
+  return (
+    <div className=" text-4xl flex flex-col gap-5">
+      <p>Hi there </p>
+      <Button className="p-5 w-25 h-10 rounded-full">Hello </Button>
+      <Badge>Hi</Badge>
+      <Input type="email" placeholder="Email" />
+      <Toggle aria-label="Toggle italic">
+        <Bold className="h-4 w-4" />
+      </Toggle>
+    </div>
+  );
 }
