04022dfdc57dca2e27a76fc3f348c14cd2eeb51b
Tamal <ckbtamaldipnew@gmail.com>
2025-03-14 00:02:23 +0600

    Add aceternity

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -4,17 +4,19 @@ import { Input } from "@/components/ui/input";
 import { Bold } from "lucide-react";
 
 import { Toggle } from "@/components/ui/toggle";
+import { ThreeDCardDemo } from "@/components/Card";
 
 export default function Home() {
   return (
     <div className=" text-4xl flex flex-col gap-5">
-      <p>Hi there </p>
-      <Button className="p-5 w-25 h-10 rounded-full">Hello </Button>
-      <Badge>Hi</Badge>
+      <p className="">Hi there </p>
+      <Button className="p-10 w-25 h-10 rounded-full">Hello </Button>
+      <Badge className="p-5">Hi</Badge>
       <Input type="email" placeholder="Email" />
       <Toggle aria-label="Toggle italic">
         <Bold className="h-4 w-4" />
       </Toggle>
+      <ThreeDCardDemo />
     </div>
   );
 }
