3f6071632e39cca4aa24df70948c163d4e24e53a
Tamal <ckbtamaldipnew@gmail.com>
2025-03-16 02:17:21 +0600

    Hero Section -> UI -> add hero scrollable image

---
+++ apps/frontend/src/src/components/HeroScrollImage.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import Image from "next/image";
+
+export function HeroScrollDemo() {
+  return (
+    <div className="flex flex-col overflow-hidden z-10">
+      <ContainerScroll titleComponent={<></>}>
+        <Image
+          src="/Images/banner.png"
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
--- apps/frontend/src/src/components/HeroSection.tsx
+++ apps/frontend/src/src/components/HeroSection.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Button } from "./ui/button";
-import Connect from "./AnimatedButtons/Connect";
 import TopGradient from "./AnimatedButtons/TopGradient";
+import { HeroScrollDemo } from "./HeroScrollImage";
 
 const HeroSection = () => {
   return (
     <main className="overflow-hidden text-slate-300">
       <div className="relative py-24 lg:py-28">
-        <div className="mx-auto max-w-7xl px-6 md:px-12">
-          <div className="text-center sm:mx-auto sm:w-10/12 lg:mr-auto lg:mt-0 lg:w-4/5 ">
+        <div className=" flex flex-col">
+          <div className="text-center sm:mx-auto sm:w-10/12 lg:mr-auto lg:mt-0 lg:w-4/5 -mb-44 z-50">
             <h1 className="mt-8 text-2xl font-semibold lg:text-6xl md:text-5xl">
               A Step Towards Freedom
             </h1>
@@ -21,6 +20,7 @@ const HeroSection = () => {
               <TopGradient text="Trade Now" />
             </div>
           </div>
+          <HeroScrollDemo />
         </div>
       </div>
     </main>
