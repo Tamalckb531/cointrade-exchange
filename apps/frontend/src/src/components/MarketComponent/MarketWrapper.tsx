5efbd3c743d3ea8eca0dfa74f17f0cc180935843
Tamal <ckbtamaldipnew@gmail.com>
2025-03-19 20:20:04 +0600

    Market -> UI -> add pagination

--- apps/frontend/src/src/components/MarketComponent/MarketWrapper.tsx
+++ apps/frontend/src/src/components/MarketComponent/MarketWrapper.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import StatWrapper from "../StatWrapper";
 import DataTable from "../TableComponent/DataTable";
+import PaginationComp from "./PaginationComp";
 
 const MarketWrapper = () => {
   return (
     <div className=" flex flex-col items-center justify-center text-white mt-80">
       <StatWrapper />
       <DataTable pageMarket={true} />
+      <div className="my-5">
+        <PaginationComp />
+      </div>
     </div>
   );
 };
---
+++ apps/frontend/src/src/components/MarketComponent/PaginationComp.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+const PaginationComp = () => {
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink>1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink>2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink>3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink>30</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+};
+
+export default PaginationComp;
