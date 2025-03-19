f712a2353100472d6abbba245850907b68346f6b
Tamal <ckbtamaldipnew@gmail.com>
2025-03-19 11:46:52 +0600

    Market -> UI -> fix datatable overflow issue

--- apps/frontend/src/app/market/page.tsx
+++ apps/frontend/src/app/market/page.tsx
@@ -3,7 +3,8 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className=" flex items-center justify-center">
+    <div>
+      {" "}
       <MarketWrapper />
     </div>
   );
--- apps/frontend/src/src/components/HeaderComponent/SearchBox.tsx
+++ apps/frontend/src/src/components/HeaderComponent/SearchBox.tsx
@@ -7,7 +7,7 @@ interface search {
 
 const SearchBox = ({ pageMarket }: search) => {
   return (
-    <div className="flex items-center justify-between flex-row  md:w-[340px] md:max-w-[340px] w-[150px] flex-1 cursor-pointer overflow-hidden rounded-xl  bg-[#202127] md:p-1 text-slate-400">
+    <div className="flex items-center justify-between flex-row  md:w-[340px] md:max-w-[340px] max-w-[150px] w-[150px] flex-1 cursor-pointer overflow-hidden rounded-xl  bg-[#202127] md:p-1 text-slate-400">
       <div className="flex items-center flex-row flex-1">
         <div className="mx-2 hidden md:block">
           <Search size={18} />
